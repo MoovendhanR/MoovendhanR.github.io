ecb92aedc977d6eb2f3a000bfd296cd5d0386980
MoovendhanR <moovendhanr1011@gmail.com>
2022-11-16 18:36:04 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,40 +1,59 @@
-import React, { useState, useEffect } from "react";
-import Preloader from "../src/components/Pre";
-import Navbar from "./components/Navbar";
-import Home from "./components/Home/Home";
-import Home3 from "./components/Home/Home3";
-import About from "./components/About/About";
-import Projects from "./components/Projects/Projects";
-import Footer from "./components/Footer";
-import { BrowserRouter as Router } from "react-router-dom";
-import "./style.css";
-import "./App.css";
-import "bootstrap/dist/css/bootstrap.min.css";
-
-import ScrollToTop from "./components/ScrollToTop";
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'font-awesome/css/font-awesome.min.css';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Home, About, Skills, Experience, Contact, Footer } from './components'
+import { useState } from "react";
+import Navbar from 'react-bootstrap/Navbar'
+import Nav from 'react-bootstrap/Nav'
+import Button from 'react-bootstrap/Button'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faAdjust } from '@fortawesome/free-solid-svg-icons'
+import './components/styles.css'
 
 function App() {
-  const [load, upadateLoad] = useState(true);
-  
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      upadateLoad(false);
-    }, 1200);
-    
-    return () => clearTimeout(timer);
-  }, []);
+
+  const [darkMode, setDarkMode] = useState(true);
+  const toggleDarkMode = () => setDarkMode(darkMode ? false : true);
 
   return (
     <Router>
-      <Preloader load={load} />
-      <div className="App" id={load ? "no-scroll" : "scroll"}>
-        <Navbar />
-        <Home />
-        <ScrollToTop />
-         <About />
-         <Projects/>
-         <Home3/>
-         <Footer />
+      <div className="App" data-theme={darkMode ? "dark" : "light"}>
+        <Route path="/">
+          <Navbar expand="lg" className="navbar" data-theme={darkMode ? "light" : "dark"}>
+            <Navbar.Brand href="#home" className="navname">Welcome!</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="mr-auto">
+                    <Nav.Link href="#about" className="link">About me</Nav.Link>
+                    <Nav.Link href="#skills" className="link">Skills</Nav.Link>
+                    <Nav.Link href="#experience" className="link">Experience</Nav.Link>
+                    <Nav.Link href="#contact" className="link">Contact</Nav.Link>
+                </Nav>
+                <Button onClick={toggleDarkMode} variant={darkMode ? "outline-light" : "outline-dark"}>
+                    {darkMode ? "Light Mode" : "Dark Mode"} <FontAwesomeIcon className="dark" size="lg" icon={faAdjust} spin/>
+                </Button>
+            </Navbar.Collapse>
+          </Navbar>
+          <hr></hr>
+          <div id="home">
+          <Home darkMode={darkMode} />
+          </div>
+          <div id="about">
+          <About darkMode={darkMode} />
+          </div>
+          <div id="skills">
+          <Skills darkMode={darkMode} />
+          </div>
+          <div id="experience">
+          <Experience darkMode={darkMode} />
+          </div>
+          <div id="contact">
+          <Contact darkMode={darkMode} />  
+          </div>
+          <Footer />
+          </Route>
+          <br></br>
       </div>
     </Router>
   );
---
+++ src/components/Footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <div>
+            <h4 className="footer-text">Designed and Developed by Moovendhan R<br></br>
+            <br></br>
+            <small>All rights reserved © 2021 Moovendhan R</small><br></br>
+
+            </h4>
+        </div>
+    )
+}
+
+export default Footer
